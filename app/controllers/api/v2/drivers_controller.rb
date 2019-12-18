daad69cc8c139ae1b6ca2b7a5819009450768cfd
Sandeep Chourey <25004643@mahindra.com>
2019-12-18 11:17:38 +0530

    Fix other docs nil

--- app/controllers/api/v2/drivers_controller.rb
+++ app/controllers/api/v2/drivers_controller.rb
@@ -77,7 +77,10 @@ class API::V2::DriversController < ApplicationController
             upload_bgc_doc(@driver) if @driver.present?
             upload_medically_certified_doc(@driver) if @driver.present?
             upload_other_docs_url(@driver) if @driver.present?
-            @driver.update(other_docs_url: nil) if @driver.other_doc.blank? 
+            ### Nil for blank doc
+            @driver.update(other_docs_url: nil) if params[:other_doc].blank?
+            @driver.update(bgc_doc_url: nil) if params[:bgc_doc].blank?
+          ### End  Nil for blank doc
             @driver.update(induction_status: "Registered")
             @driver.update(compliance_status: "Ready For Allocation")
             @driver.update(date_of_registration: Time.now )
