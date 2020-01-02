f65109f7650b42edfa05ce34db1d2b0cdabf4b0f
Umar Sayyed <sayyed25507@gmail.com>
2020-01-02 15:44:52 +0530

    trip board changes

--- app/assets/javascripts/angular/controller/tripBoardController.js
+++ app/assets/javascripts/angular/controller/tripBoardController.js
@@ -141,9 +141,9 @@ angular.module('app').controller('tripboardCtrl', function ($scope, VehicleListR
 
   $scope.tripOngoing = true;
 
-  function makeMarker(position, title) {
+  function makeMarker(position, title, isSite = false) {
     // var image = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2238%22%20height%3D%2238%22%20viewBox%3D%220%200%2038%2038%22%3E%3Cpath%20fill%3D%22%23808080%22%20stroke%3D%22%23ccc%22%20stroke-width%3D%22.5%22%20d%3D%22M34.305%2016.234c0%208.83-15.148%2019.158-15.148%2019.158S3.507%2025.065%203.507%2016.1c0-8.505%206.894-14.304%2015.4-14.304%208.504%200%2015.398%205.933%2015.398%2014.438z%22%2F%3E%3Ctext%20transform%3D%22translate%2819%2018.5%29%22%20fill%3D%22%23fff%22%20style%3D%22font-family%3A%20Arial%2C%20sans-serif%3Bfont-weight%3Abold%3Btext-align%3Acenter%3B%22%20font-size%3D%2212%22%20text-anchor%3D%22middle%22%3E' + 'umar' + '%3C%2Ftext%3E%3C%2Fsvg%3E';
-    if (title != null && title.length > 0 && title !== 'Vaayu Site') {
+    if (!isSite) {
       return showCustomMarker(position, title, title)
     }
     return new google.maps.Marker({
@@ -206,11 +206,11 @@ angular.module('app').controller('tripboardCtrl', function ($scope, VehicleListR
     
     // clearMarkers(map_markers);
     if (source.is_site) {
-      map_markers.push(makeMarker(new google.maps.LatLng(source.lat, source.lng), source.site_name));
+      map_markers.push(makeMarker(new google.maps.LatLng(source.lat, source.lng), source.site_name, true));
     }
 
     if (destination.is_site) {
-      map_markers.push(makeMarker(new google.maps.LatLng(destination.lat, destination.lng), destination.site_name));
+      map_markers.push(makeMarker(new google.maps.LatLng(destination.lat, destination.lng), destination.site_name, true));
     }
 
     directionsService.route({
