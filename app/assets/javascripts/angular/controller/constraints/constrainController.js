7a228b0ab554b21f66155a4c05784ae59672d1a7
= <samariya.praveen@mahindra.com>
2020-01-07 19:04:11 +0530

    added testing domain in angular

--- app/assets/javascripts/angular/controller/constraints/constrainController.js
+++ app/assets/javascripts/angular/controller/constraints/constrainController.js
@@ -39,7 +39,7 @@ app.controller('constraintController', function ($scope, $http, $state, SessionS
 
     $http({
       method: 'GET',
-      url: 'http://vaayuuat.devmll.com/' + 'constraint/getall/site/' + id,
+      url: 'http://operatorptsdemo.devmll.com/' + 'constraint/getall/site/' + id,
       headers: {
         // 'Content-Type': 'application/json',
         'uid': SessionService.uid,
@@ -101,7 +101,7 @@ app.controller('constraintController', function ($scope, $http, $state, SessionS
 
     $http({
       method: 'POST',
-      url: 'http://vaayuuat.devmll.com:8001/api/v1/getAllSiteList',
+      url: 'http://operatorptsdemo.devmll.com:8001/api/v1/getAllSiteList',
       headers: {
         'Content-Type': 'application/json',
         'uid': SessionService.uid,
