d456b47175abc2b27a9a710d89ebd2dab3d67c36
= <samariya.praveen@mahindra.com>
2020-01-21 16:55:57 +0530

    added

--- app/assets/javascripts/angular/services/commonService.js
+++ app/assets/javascripts/angular/services/commonService.js
@@ -1,16 +1,16 @@
 
 // Services 
-angular.module('app').constant('BASE_URL', 'http://api.mllvaayu.com:8001/api/v1/');
-angular.module('app').constant('BASE_URL_8002', 'http://api.mllvaayu.com/');
-angular.module('app').constant('BASE_URL_API_8002', 'http://api.mllvaayu.com:8002/api/v1/');
-angular.module('app').constant('BASE_URL_API_8003', 'http://api.mllvaayu.com:8003/api/v1/');
-angular.module('app').constant('BASE_URL_RUBY', 'http://operator.mllvaayu.com/api/v2/');
+// angular.module('app').constant('BASE_URL', 'http://api.mllvaayu.com:8001/api/v1/');
+// angular.module('app').constant('BASE_URL_8002', 'http://api.mllvaayu.com/');
+// angular.module('app').constant('BASE_URL_API_8002', 'http://api.mllvaayu.com:8002/api/v1/');
+// angular.module('app').constant('BASE_URL_API_8003', 'http://api.mllvaayu.com:8003/api/v1/');
+// angular.module('app').constant('BASE_URL_RUBY', 'http://operator.mllvaayu.com/api/v2/');
 
-// angular.module('app').constant('BASE_URL', 'http://apiptsdemo.devmll.com:8001/api/v1/');
-// angular.module('app').constant('BASE_URL_8002', 'http://apiptsdemo.devmll.com/');
-// angular.module('app').constant('BASE_URL_API_8002', 'http://apiptsdemo.devmll.com:8002/api/v1/');
-// angular.module('app').constant('BASE_URL_API_8003', 'http://apiptsdemo.devmll.com:8003/api/v1/');
-// angular.module('app').constant('BASE_URL_RUBY', 'http://apiptsdemo.devmll.com/api/v2/');
+angular.module('app').constant('BASE_URL', 'http://apiptsdemo.devmll.com:8001/api/v1/');
+angular.module('app').constant('BASE_URL_8002', 'http://apiptsdemo.devmll.com/');
+angular.module('app').constant('BASE_URL_API_8002', 'http://apiptsdemo.devmll.com:8002/api/v1/');
+angular.module('app').constant('BASE_URL_API_8003', 'http://apiptsdemo.devmll.com:8003/api/v1/');
+angular.module('app').constant('BASE_URL_RUBY', 'http://apiptsdemo.devmll.com/api/v2/');
 
 angular.module('app').factory('SessionService', function() {
     // var _data ={
