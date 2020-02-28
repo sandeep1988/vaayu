95ca55f6761c97f241552aea00daff3dea416ab9
= <samariya.praveen@mahindra.com>
2020-02-28 09:51:36 +0530

    Routes finalize toaster message added

--- app/assets/javascripts/angular/controller/routerController.js
+++ app/assets/javascripts/angular/controller/routerController.js
@@ -464,6 +464,8 @@ angular.module('app').controller('routeCtrl', function ($scope, $http, $state, M
       FinalizeService.query(postdata, (data) => {
         $scope.resetRoute();
         $scope.allRouteSelected=false;
+        $scope.toggleView = true;
+        ToasterService.showSuccess('Success', 'Routes are finalized')
       }, err => {
         $scope.toggleView = true;
         ToasterService.showError('Error', 'Something went wrong')
