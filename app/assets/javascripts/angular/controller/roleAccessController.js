238816bd13bc62acea957e16d20bb7bd7ba1048f
Unmesh Gavade <gavade.unmesh@mahindra.com>
2020-03-05 15:36:51 +0530

    role access table model

--- app/assets/javascripts/angular/controller/roleAccessController.js
+++ app/assets/javascripts/angular/controller/roleAccessController.js
@@ -4,9 +4,27 @@ angular.module('app').controller('roleAccessCtrl', function ($scope, RosterServi
         $scope.getTableData();
     }
 
+
     $scope.tableRow = [];
     $scope.moduleColumn = []
     $scope.roleModuleAccess = [];
+
+    $scope.postData = {
+        "roleAccess": []
+    }
+
+    $scope.roleObj = {
+        role_id : 2,
+        module_id : 1,
+        delete_status:0,
+        edit_status:1,
+        view_status:1
+    }
+    
+    
+    
+    
+    
 
     $scope.getTableData = () => {
         RosterService.userRoles((res) => {
@@ -17,8 +35,17 @@ angular.module('app').controller('roleAccessCtrl', function ($scope, RosterServi
         })
     }
 
-    $scope.onCheckboxChange = (index) => {
+    $scope.onCheckboxChange = (index, userRole, moduleName, status) => {
+        console.log(index, userRole, moduleName, status)
+        // get the role_id and module_id, toggle values of the element from id
+    }
+
+    $scope.onSave = (index) => {
         console.log(index)
+    }
+
+    $scope.ObjPush = (index) => {
+       console.log(index) 
     }
 
 })
