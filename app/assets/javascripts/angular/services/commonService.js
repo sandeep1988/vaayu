9591c28ffb1a9e6fdf319bf75046a3cd9e0eba59
Unmesh Gavade <gavade.unmesh@mahindra.com>
2020-03-11 12:09:09 +0530

    changes in common service for API endpoints

--- app/assets/javascripts/angular/services/commonService.js
+++ app/assets/javascripts/angular/services/commonService.js
@@ -43,6 +43,17 @@ if (document.domain === PROD_OPERATOR_URL) {
     angular.module('app').constant('BASE_URL_API_8005', NODE_API_URL+':8005/api/v1/');
     angular.module('app').constant('BASE_URL_RUBY', 'http://'+QA_OPERATOR_URL+'/api/v2/'); 
 
+} else if (document.domain === SECURITY_OPERATOR_URL) {
+    // let NODE_API_URL = 'http://apiptsdemo.devmll.com';
+    let NODE_API_URL = 'http://operatorptsdemo.devmll.com';
+    angular.module('app').constant('BASE_URL', NODE_API_URL+':8001/api/v1/');
+    angular.module('app').constant('BASE_URL_8002', NODE_API_URL + '/');
+    angular.module('app').constant('BASE_URL_API_8002', NODE_API_URL+':8002/api/v1/');
+    angular.module('app').constant('BASE_URL_API_8003', NODE_API_URL+':8003/api/v1/');
+    angular.module('app').constant('BASE_URL_API_8004', NODE_API_URL+':8004/api/v1/');
+    angular.module('app').constant('BASE_URL_API_8005', NODE_API_URL+':8005/api/v1/');
+    angular.module('app').constant('BASE_URL_RUBY', 'http://'+SECURITY_OPERATOR_URL+'/api/v2/'); 
+    
 } else {//if (document.domain === SECURITY_OPERATOR_URL) 
     // let NODE_API_URL = 'http://apiptsdemo.devmll.com';
     let NODE_API_URL = 'http://qaapi.mllvaayu.com';
