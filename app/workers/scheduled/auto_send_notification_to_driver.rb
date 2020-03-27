bc161e03cf979f746eea7254f43907cba44c1738
Dhruv Sharma <sharma.dhruv@mahindra.com>
2020-03-27 14:08:11 +0530

    Added SMS Functionality

--- app/workers/scheduled/auto_send_notification_to_driver.rb
+++ app/workers/scheduled/auto_send_notification_to_driver.rb
@@ -11,7 +11,6 @@ class Scheduled::AutoSendNotificationToDriver
 			 planned_date = trip.planned_date + 5.hours + 30.minutes 
 			 time_flag = Time.now >= (trip.planned_date + 4.hours + 30.minutes) && Time.now < (trip.planned_date + 5.hours + 30.minutes)
 			 if time_flag
-			 trip = Trip.find(154879)
 				check_notification_status(trip)
 			 else
 				puts "Not in an hour range for #{planned_date}, with respect to current time #{Time.now}"
@@ -32,12 +31,17 @@ class Scheduled::AutoSendNotificationToDriver
 
 	def notification_reminder(trip)
 	 	user_id = Driver.find(trip.driver_id).user_id
+	 	@user = User.find(user_id)
 	 	
 		#preparing JSON for Mobile
 		data = {driver_id: user_id, data: {driver_id: user_id, trip_id: trip.id, planned_date: trip.planned_date, push_type: :hourly_before_trip }}
 		data.merge!(notification: { title: "Next Trip Reminder", body: "Notification Text:Your next trip #{trip.id} is scheduled for #{trip.planned_date + 5.hours + 30.minutes}, please visit \"Upcoming Trips\" menu to take action on the same." })
+		
 		#send notification
 		PushNotificationWorker.perform_async(user_id, :hourly_before_trip, data)
+		
+		#send SMS
+		SMSWorker.perform_async(@user.phone, ENV['OPERATOR_NUMBER'], "Your next trip #{trip.id} is scheduled for #{trip.planned_date + 5.hours + 30.minutes}, please visit Alyte Driver mobile app to take action on the same.");
 		
 		#update in notification table that notification is sent
 		Notification.create!(message: "notify driver an hour before", trip_id: trip.id, driver_id: trip.driver_id, status: "created")
