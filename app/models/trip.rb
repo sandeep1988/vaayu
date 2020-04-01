e6dc3e0799191ec5994f78e1ef2d3c586ec791f4
MLL25007990 <62328814+MLL25007990@users.noreply.github.com>
2020-04-01 22:36:55 +0530

    checking commit not displayed

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1378,7 +1378,7 @@ class Trip < ApplicationRecord
 		#writing notification flag logic here end here
 	   puts @flag
      # abort(@flag)
-     puts "===========flag=============="
+     puts "===========flag1=============="
      data = {
             data: {
                 trip_id: self.id,
