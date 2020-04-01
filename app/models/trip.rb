ec70a275092e0142879b542a5865ec48f73564a4
Sandeep Chourey <25004643@mahindra.com>
2020-04-01 14:45:15 +0530

    Fixed remove create status for notification

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1326,7 +1326,7 @@ class Trip < ApplicationRecord
       #  @flag = true 
 		#writing notification flag logic here starts here
 		current_trip_id = self.id
-		trips = Trip.where("(status = ? OR status = ? OR status = ? OR status = ? OR status = ?) AND driver_id = ?", 'assinged', 'assign_requested', 'assign_request_expired','created', 'active', driver.id).order(:planned_date)
+		trips = Trip.where("(status = ? OR status = ? OR status = ? OR status = ?) AND driver_id = ?", 'assinged', 'assign_requested', 'assign_request_expired', 'active', driver.id).order(:planned_date)
 
 	 p "========trip count============"
    p trips.count
