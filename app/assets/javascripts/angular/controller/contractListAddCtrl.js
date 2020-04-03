c933c3f95ea9b5c2e4ebdbd077b62d31f3b6fe4a
Unmesh Gavade <gavade.unmesh@mahindra.com>
2020-04-03 12:37:16 +0530

    changes and bug fixes of zone and contract Toast message

--- app/assets/javascripts/angular/controller/contractListAddCtrl.js
+++ app/assets/javascripts/angular/controller/contractListAddCtrl.js
@@ -1,6 +1,7 @@
 app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_MAIN, BASE_URL_API_8003,SessionService, ToasterService, ContractService, $timeout) {
 
     this.$onInit = function () {
+        $scope.toggleView = true;
         console.log('onit - contractListAddCtrl');
         $scope.totalSelectedUIDs = "Select UIDs";
         $scope.toggleView = false;
@@ -262,32 +263,26 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
     $scope.isValid = () => {
         if ($scope.selectedUIDs.length == 0) {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Select one or more UDID\'s');
             return false;
         } else if (!$scope.selectedSiteId && $scope.tab == 'CUSTOMER') {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Select Site');
             return false;
         } else if (!$scope.baID && $scope.tab == 'BA') {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Select BA');
             return false;
         } else if (!$scope.bcycle) {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Select Billing Cycle.');
             return false;
         } else if (!$scope.ctype) {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Select Contract Type.');
             return false;
         } else if (!$scope.fileObject) {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Upload contract in csv');
             return false;
         }
@@ -329,7 +324,6 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
                     vm.submitResponse = JSON.parse(request.response);
                     if (vm.submitResponse['success']) {
                         $scope.toggleView = true;
-                        ToasterService.clearToast();
                         ToasterService.showSuccess('Success', 'Contract created successfully.');
                         console.log('Contract created successfully.');
                         $scope.getContracts();
@@ -351,11 +345,9 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
                                 }
                             }
                             $scope.toggleView = true;
-                            ToasterService.clearToast();
                             ToasterService.showError_html('Error', msg.replace(/(\r\n|\n|\r)/gm, "<br>"));
                         } else {
                             $scope.toggleView = true;
-                            ToasterService.clearToast();
                             ToasterService.showError_html('Error', vm.submitResponse['message']);
                         }
                     }
@@ -363,7 +355,6 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
                 }
             } else {
                 $scope.toggleView = true;
-                ToasterService.clearToast();
                 ToasterService.showError('Error', 'Something went wrong, Try again later.');
             }
         };
@@ -488,12 +479,10 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
         var type = 'SITE'
         if (!$scope.selectedSiteId && $scope.tab === 'CUSTOMER') {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Please select SITE name');
             return;
         } else if (!$scope.baID && $scope.tab === 'BA') {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Please select BA name');
             id = $scope.baID
             type = 'BA'
@@ -536,7 +525,6 @@ app.controller('contractListAddCtrl', function ($scope, $http, $state,BASE_URL_M
 
         }, er => {
             $scope.toggleView = true;
-            ToasterService.clearToast();
             ToasterService.showError('Error', 'Something went wrong, Try again later.');
             console.log(err)
         });
