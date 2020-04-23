667eb7c081a089b2413684131d8f4bd333409716
Sandeep Chourey <25004643@mahindra.com>
2020-04-23 15:29:54 +0530

    Fixed error for employee company

--- app/controllers/employee_companies_controller.rb
+++ app/controllers/employee_companies_controller.rb
@@ -51,7 +51,8 @@ class EmployeeCompaniesController < ApplicationController
           @employee_company.update(active: company_params[:active] == "Inactive" ? false : true)
           format.json { render json: EmployeeCompanyDatatable.new(@employee_company), status: :created, location: @employee_companies}
         else
-          format.json { render json: @employee_company.errors.messages, status: '404' }
+          format.json { render json: { success: false , errors:  @employee_company.errors.full_messages }, status: '200' }
+          # format.json { render json: @employee_company.errors.messages, status: '404' }
         end
       end
     else
@@ -69,7 +70,8 @@ class EmployeeCompaniesController < ApplicationController
           @employee_company.update(active: company_params[:active] == "Inactive" ? false : true)
           format.json { render json: EmployeeCompanyDatatable.new(@employee_company), status: :ok, location: @employee_company }
         else
-          format.json { render json: @employee_company.errors, status: :unprocessable_entity }
+          # format.json { render json: @employee_company.errors, status: :unprocessable_entity }
+          format.json { render json: { success: false , errors:  @employee_company.errors.full_messages }, status: '200' }
         end
       end
     else
